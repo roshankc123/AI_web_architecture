08bd159e0bd0517b202dbdb7db23ee9eb2fa0623
Cruserap1 <cruserap@gmail.com>
2022-12-08 18:06:39 +0545

    anjan main updated

--- static/js/main.js
+++ static/js/main.js
@@ -7,13 +7,15 @@ const canvas = document.querySelector("canvas");
 const screenshotImage = document.querySelector(".screenshot-image");
 const buttons = [...controls.querySelectorAll("button")];
 
-const submit=document.querySelector(".finalSubmit")
-const submitButtons=[...submit.querySelectorAll("button")]
+const submit = document.querySelector(".finalSubmit");
+const submitButtons = [...submit.querySelectorAll("button")];
 const [submitData] = submitButtons;
 
 let streamStarted = false;
 
 const [play, pause, screenshot] = buttons;
+
+let variables={}
 
 const constraints = {
   video: {
@@ -90,33 +92,57 @@ const pauseStream = () => {
 };
 
 const doScreenshot = () => {
-  canvas.width = video.videoWidth/3;
-  canvas.height = video.videoHeight/3;
+  canvas.width = video.videoWidth / 3;
+  canvas.height = video.videoHeight / 3;
   console.log("screenshot");
-  canvas.getContext("2d").drawImage(video, 0, 0,video.videoWidth/3, video.videoHeight/3);
-  screenshotImage.src = canvas.toDataURL("image/jpg",0.7);
+  console.log("screenshot");
+  canvas
+    .getContext("2d")
+    .drawImage(video, 0, 0, video.videoWidth / 3, video.videoHeight / 3);
+  screenshotImage.src = canvas.toDataURL("image/jpg", 0.7);
 
+  var binStr = atob(canvas.toDataURL("image/png", 0.2).split(",")[1]),
+    len = binStr.length,
+    arr = new Uint8Array(len);
+  for (var i = 0; i < len; i++) {
+    arr[i] = binStr.charCodeAt(i);
+  }
 
-//   var binStr = atob(canvas.toDataURL("image/png", 0.2).split(',')[1]),
-//   len = binStr.length,
-//   arr = new Uint8Array(len);
+  let blob = new Blob([arr], { type: "image/png" });
 
-  console.log(screenshotImage);
+  // console.log(screenshotImage);
+  console.log(blob);
+  variables.blob=blob
+  variables.arr=arr
   screenshotImage.classList.remove("d-none");
 };
 
-const submitAction = async () => {
+const submitAction = async (e) => {
+  e.preventDefault();
 
-  const src=screenshotImage.src.split(',')
-  const form = document.forms.namedItem('mainForm');
-  // console.log(form)
-  let formData = new FormData(form);
-  formData.append("image", src[1]);
-  getData(formData)
+  const formData = new FormData();
 
-  console.log("invoked")
+  formData.append("name", "UserName");
 
- 
+
+  // JavaScript file-like object
+  const content = '<q id="a"><span id="b">hey!</span></q>'; // the body of the new file…
+  const blob = new Blob([variables.arr], { type: "text/xml" });
+  console.log(blob)
+  formData.append("file1", blob);
+
+  const request = new XMLHttpRequest();
+  request.open("POST", "{{url_for('after')}}");
+  request.send(formData);
+
+  // const src = screenshotImage;
+  // const form = document.forms.namedItem("mainForm");
+  // console.log(form);
+  // let formData = new FormData(form);
+  // formData.append("image", src[1]);
+  // getData(formData);
+  // console.log(formData);
+  // console.log("invoked");
 };
 
 const getData = async (formData) => {
@@ -136,134 +162,160 @@ const getData = async (formData) => {
 
 
 
-  // From https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob, needed for Safari:
-if (!HTMLCanvasElement.prototype.toBlob) {
-  Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
-      value: function(callback, type, quality) {
-
-          var binStr = atob(this.toDataURL(type, quality).split(',')[1]),
-              len = binStr.length,
-              arr = new Uint8Array(len);
-
-          for (var i = 0; i < len; i++) {
-              arr[i] = binStr.charCodeAt(i);
-          }
-
-          callback(new Blob([arr], {type: type || 'image/png'}));
-      }
-  });
-}
-
-window.URL = window.URL || window.webkitURL;
-
-// Modified from https://stackoverflow.com/a/32490603, cc by-sa 3.0
-// -2 = not jpeg, -1 = no data, 1..8 = orientations
-function getExifOrientation(file, callback) {
-  // Suggestion from http://code.flickr.net/2012/06/01/parsing-exif-client-side-using-javascript-2/:
-  if (file.slice) {
-      file = file.slice(0, 131072);
-  } else if (file.webkitSlice) {
-      file = file.webkitSlice(0, 131072);
-  }
-
-  var reader = new FileReader();
-  reader.onload = function(e) {
-      var view = new DataView(e.target.result);
-      if (view.getUint16(0, false) != 0xFFD8) {
-          callback(-2);
-          return;
-      }
-      var length = view.byteLength, offset = 2;
-      while (offset < length) {
-          var marker = view.getUint16(offset, false);
-          offset += 2;
-          if (marker == 0xFFE1) {
-              if (view.getUint32(offset += 2, false) != 0x45786966) {
-                  callback(-1);
-                  return;
-              }
-              var little = view.getUint16(offset += 6, false) == 0x4949;
-              offset += view.getUint32(offset + 4, little);
-              var tags = view.getUint16(offset, little);
-              offset += 2;
-              for (var i = 0; i < tags; i++)
-                  if (view.getUint16(offset + (i * 12), little) == 0x0112) {
-                      callback(view.getUint16(offset + (i * 12) + 8, little));
-                      return;
-                  }
-          }
-          else if ((marker & 0xFF00) != 0xFF00) break;
-          else offset += view.getUint16(offset, false);
-      }
-      callback(-1);
-  };
-  reader.readAsArrayBuffer(file);
-}
-
-// Derived from https://stackoverflow.com/a/40867559, cc by-sa
-function imgToCanvasWithOrientation(img, rawWidth, rawHeight, orientation) {
-  var canvas = document.createElement('canvas');
-  if (orientation > 4) {
-      canvas.width = rawHeight;
-      canvas.height = rawWidth;
-  } else {
-      canvas.width = rawWidth;
-      canvas.height = rawHeight;
-  }
-
-  if (orientation > 1) {
-      console.log("EXIF orientation = " + orientation + ", rotating picture");
-  }
-
-  var ctx = canvas.getContext('2d');
-  switch (orientation) {
-      case 2: ctx.transform(-1, 0, 0, 1, rawWidth, 0); break;
-      case 3: ctx.transform(-1, 0, 0, -1, rawWidth, rawHeight); break;
-      case 4: ctx.transform(1, 0, 0, -1, 0, rawHeight); break;
-      case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
-      case 6: ctx.transform(0, 1, -1, 0, rawHeight, 0); break;
-      case 7: ctx.transform(0, -1, -1, 0, rawHeight, rawWidth); break;
-      case 8: ctx.transform(0, -1, 1, 0, 0, rawWidth); break;
-  }
-  ctx.drawImage(img, 0, 0, rawWidth, rawHeight);
-  return canvas;
-}
-
-function reduceFileSize(file, acceptFileSize, maxWidth, maxHeight, quality, callback) {
-  if (file.size <= acceptFileSize) {
-      callback(file);
-      return;
-  }
-  var img = new Image();
-  img.onerror = function() {
-      URL.revokeObjectURL(this.src);
-      callback(file);
-  };
-  img.onload = function() {
-      URL.revokeObjectURL(this.src);
-      getExifOrientation(file, function(orientation) {
-          var w = img.width, h = img.height;
-          var scale = (orientation > 4 ?
-              Math.min(maxHeight / w, maxWidth / h, 1) :
-              Math.min(maxWidth / w, maxHeight / h, 1));
-          h = Math.round(h * scale);
-          w = Math.round(w * scale);
-
-          var canvas = imgToCanvasWithOrientation(img, w, h, orientation);
-          canvas.toBlob(function(blob) {
-              console.log("Resized image to " + w + "x" + h + ", " + (blob.size >> 10) + "kB");
-              callback(blob);
-          }, 'image/jpeg', quality);
-      });
-  };
-  img.src = URL.createObjectURL(file);
-}
 
 
 
 
+// From https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob, needed for Safari:
+// if (!HTMLCanvasElement.prototype.toBlob) {
+//   Object.defineProperty(HTMLCanvasElement.prototype, "toBlob", {
+//     value: function (callback, type, quality) {
+//       var binStr = atob(this.toDataURL(type, quality).split(",")[1]),
+//         len = binStr.length,
+//         arr = new Uint8Array(len);
 
+//       for (var i = 0; i < len; i++) {
+//         arr[i] = binStr.charCodeAt(i);
+//       }
 
+//       callback(new Blob([arr], { type: type || "image/png" }));
+//     },
+//   });
+// }
+
+// window.URL = window.URL || window.webkitURL;
+
+// // Modified from https://stackoverflow.com/a/32490603, cc by-sa 3.0
+// // -2 = not jpeg, -1 = no data, 1..8 = orientations
+// function getExifOrientation(file, callback) {
+//   // Suggestion from http://code.flickr.net/2012/06/01/parsing-exif-client-side-using-javascript-2/:
+//   if (file.slice) {
+//     file = file.slice(0, 131072);
+//   } else if (file.webkitSlice) {
+//     file = file.webkitSlice(0, 131072);
+//   }
+
+//   var reader = new FileReader();
+//   reader.onload = function (e) {
+//     var view = new DataView(e.target.result);
+//     if (view.getUint16(0, false) != 0xffd8) {
+//       callback(-2);
+//       return;
+//     }
+//     var length = view.byteLength,
+//       offset = 2;
+//     while (offset < length) {
+//       var marker = view.getUint16(offset, false);
+//       offset += 2;
+//       if (marker == 0xffe1) {
+//         if (view.getUint32((offset += 2), false) != 0x45786966) {
+//           callback(-1);
+//           return;
+//         }
+//         var little = view.getUint16((offset += 6), false) == 0x4949;
+//         offset += view.getUint32(offset + 4, little);
+//         var tags = view.getUint16(offset, little);
+//         offset += 2;
+//         for (var i = 0; i < tags; i++)
+//           if (view.getUint16(offset + i * 12, little) == 0x0112) {
+//             callback(view.getUint16(offset + i * 12 + 8, little));
+//             return;
+//           }
+//       } else if ((marker & 0xff00) != 0xff00) break;
+//       else offset += view.getUint16(offset, false);
+//     }
+//     callback(-1);
+//   };
+//   reader.readAsArrayBuffer(file);
+// }
+
+// // Derived from https://stackoverflow.com/a/40867559, cc by-sa
+// function imgToCanvasWithOrientation(img, rawWidth, rawHeight, orientation) {
+//   var canvas = document.createElement("canvas");
+//   if (orientation > 4) {
+//     canvas.width = rawHeight;
+//     canvas.height = rawWidth;
+//   } else {
+//     canvas.width = rawWidth;
+//     canvas.height = rawHeight;
+//   }
+
+//   if (orientation > 1) {
+//     console.log("EXIF orientation = " + orientation + ", rotating picture");
+//   }
+
+//   var ctx = canvas.getContext("2d");
+//   switch (orientation) {
+//     case 2:
+//       ctx.transform(-1, 0, 0, 1, rawWidth, 0);
+//       break;
+//     case 3:
+//       ctx.transform(-1, 0, 0, -1, rawWidth, rawHeight);
+//       break;
+//     case 4:
+//       ctx.transform(1, 0, 0, -1, 0, rawHeight);
+//       break;
+//     case 5:
+//       ctx.transform(0, 1, 1, 0, 0, 0);
+//       break;
+//     case 6:
+//       ctx.transform(0, 1, -1, 0, rawHeight, 0);
+//       break;
+//     case 7:
+//       ctx.transform(0, -1, -1, 0, rawHeight, rawWidth);
+//       break;
+//     case 8:
+//       ctx.transform(0, -1, 1, 0, 0, rawWidth);
+//       break;
+//   }
+//   ctx.drawImage(img, 0, 0, rawWidth, rawHeight);
+//   return canvas;
+// }
+
+// function reduceFileSize(
+//   file,
+//   acceptFileSize,
+//   maxWidth,
+//   maxHeight,
+//   quality,
+//   callback
+// ) {
+//   if (file.size <= acceptFileSize) {
+//     callback(file);
+//     return;
+//   }
+//   var img = new Image();
+//   img.onerror = function () {
+//     URL.revokeObjectURL(this.src);
+//     callback(file);
+//   };
+//   img.onload = function () {
+//     URL.revokeObjectURL(this.src);
+//     getExifOrientation(file, function (orientation) {
+//       var w = img.width,
+//         h = img.height;
+//       var scale =
+//         orientation > 4
+//           ? Math.min(maxHeight / w, maxWidth / h, 1)
+//           : Math.min(maxWidth / w, maxHeight / h, 1);
+//       h = Math.round(h * scale);
+//       w = Math.round(w * scale);
+
+//       var canvas = imgToCanvasWithOrientation(img, w, h, orientation);
+//       canvas.toBlob(
+//         function (blob) {
+//           console.log(
+//             "Resized image to " + w + "x" + h + ", " + (blob.size >> 10) + "kB"
+//           );
+//           callback(blob);
+//         },
+//         "image/jpeg",
+//         quality
+//       );
+//     });
+//   };
+//   img.src = URL.createObjectURL(file);
+// }
 
 pause.onclick = pauseStream;
 screenshot.onclick = doScreenshot;
